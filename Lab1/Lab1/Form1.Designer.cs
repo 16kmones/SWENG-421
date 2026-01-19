6d0a36c43d9757d8d053982017979a638a4b1f1f
16kmones <16kmones@gmail.com>
2026-01-19 16:36:15 -0500

    Lab 1 Kmones

---
+++ Lab1/Lab1/Form1.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace Lab1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.redTrackBar = new System.Windows.Forms.TrackBar();
+            this.lineButton = new System.Windows.Forms.Button();
+            this.rectangleButton = new System.Windows.Forms.Button();
+            this.ellipseButton = new System.Windows.Forms.Button();
+            this.greenTrackBar = new System.Windows.Forms.TrackBar();
+            this.blueTrackBar = new System.Windows.Forms.TrackBar();
+            this.panel1 = new System.Windows.Forms.Panel();
+            ((System.ComponentModel.ISupportInitialize)(this.redTrackBar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.greenTrackBar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.blueTrackBar)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // redTrackBar
+            // 
+            this.redTrackBar.BackColor = System.Drawing.Color.Red;
+            this.redTrackBar.Location = new System.Drawing.Point(342, 12);
+            this.redTrackBar.Maximum = 255;
+            this.redTrackBar.Name = "redTrackBar";
+            this.redTrackBar.Size = new System.Drawing.Size(104, 56);
+            this.redTrackBar.TabIndex = 0;
+            // 
+            // lineButton
+            // 
+            this.lineButton.Location = new System.Drawing.Point(27, 22);
+            this.lineButton.Name = "lineButton";
+            this.lineButton.Size = new System.Drawing.Size(77, 30);
+            this.lineButton.TabIndex = 2;
+            this.lineButton.Text = "Line";
+            this.lineButton.UseVisualStyleBackColor = true;
+            this.lineButton.Click += new System.EventHandler(this.lineButton_Click);
+            // 
+            // rectangleButton
+            // 
+            this.rectangleButton.Location = new System.Drawing.Point(110, 22);
+            this.rectangleButton.Name = "rectangleButton";
+            this.rectangleButton.Size = new System.Drawing.Size(87, 30);
+            this.rectangleButton.TabIndex = 3;
+            this.rectangleButton.Text = "Rectangle";
+            this.rectangleButton.UseVisualStyleBackColor = true;
+            this.rectangleButton.Click += new System.EventHandler(this.rectangleButton_Click);
+            // 
+            // ellipseButton
+            // 
+            this.ellipseButton.Location = new System.Drawing.Point(203, 22);
+            this.ellipseButton.Name = "ellipseButton";
+            this.ellipseButton.Size = new System.Drawing.Size(81, 30);
+            this.ellipseButton.TabIndex = 4;
+            this.ellipseButton.Text = "Ellipse";
+            this.ellipseButton.UseVisualStyleBackColor = true;
+            this.ellipseButton.Click += new System.EventHandler(this.ellipseButton_Click);
+            // 
+            // greenTrackBar
+            // 
+            this.greenTrackBar.BackColor = System.Drawing.Color.Lime;
+            this.greenTrackBar.Location = new System.Drawing.Point(487, 12);
+            this.greenTrackBar.Maximum = 255;
+            this.greenTrackBar.Name = "greenTrackBar";
+            this.greenTrackBar.Size = new System.Drawing.Size(104, 56);
+            this.greenTrackBar.TabIndex = 5;
+            // 
+            // blueTrackBar
+            // 
+            this.blueTrackBar.BackColor = System.Drawing.Color.Blue;
+            this.blueTrackBar.Location = new System.Drawing.Point(627, 12);
+            this.blueTrackBar.Maximum = 255;
+            this.blueTrackBar.Name = "blueTrackBar";
+            this.blueTrackBar.Size = new System.Drawing.Size(104, 56);
+            this.blueTrackBar.TabIndex = 6;
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.panel1.Location = new System.Drawing.Point(31, 83);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(736, 348);
+            this.panel1.TabIndex = 7;
+            this.panel1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseDown);
+            this.panel1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseUp_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.blueTrackBar);
+            this.Controls.Add(this.greenTrackBar);
+            this.Controls.Add(this.ellipseButton);
+            this.Controls.Add(this.rectangleButton);
+            this.Controls.Add(this.lineButton);
+            this.Controls.Add(this.redTrackBar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.redTrackBar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.greenTrackBar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.blueTrackBar)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TrackBar redTrackBar;
+        private System.Windows.Forms.Button lineButton;
+        private System.Windows.Forms.Button rectangleButton;
+        private System.Windows.Forms.Button ellipseButton;
+        private System.Windows.Forms.TrackBar greenTrackBar;
+        private System.Windows.Forms.TrackBar blueTrackBar;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ Lab1/Lab1/Form1.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab1
+{
+    public partial class Form1 : Form
+    {
+        enum ShapeType { line, rectangle, ellipse };
+        //determine shape type, default to line
+        private ShapeType currentType = ShapeType.line;
+        private struct PreviousShapes
+        {
+            //save previous shape data for panel resizing
+            public ShapeType type;
+            public int X1, X2, Y1, Y2;
+            public Color color;
+        }
+        //store previous shape data in a list
+        private readonly List<PreviousShapes> drawings = new List<PreviousShapes>();
+
+        public Form1()
+        {
+            InitializeComponent();
+            panel1.Paint += panel1_Paint;
+        }
+     
+
+        private Color GetColor()
+        {
+            //determine color based on RGB track bars
+            return Color.FromArgb(redTrackBar.Value, greenTrackBar.Value, blueTrackBar.Value);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        int oldX, oldY;
+
+        private void lineButton_Click(object sender, EventArgs e)
+        {
+            currentType = ShapeType.line;
+        }
+
+        private void rectangleButton_Click(object sender, EventArgs e)
+        {
+            currentType = ShapeType.rectangle;
+        }
+        private void ellipseButton_Click(object sender, EventArgs e)
+        {
+            currentType = ShapeType.ellipse;
+        }
+
+        private void panel1_MouseDown(object sender, MouseEventArgs e)
+        {
+            oldX = e.X;
+            oldY = e.Y;
+        }
+
+        private Rectangle MakeRect(int x1, int y1, int x2, int y2)
+        {
+            //left coordinate
+            int x = Math.Min(x1, x2);
+            //top coordinate
+            int y = Math.Min(y1, y2);
+            //width
+            int w = Math.Abs(x2 - x1);
+            //height
+            int h = Math.Abs(y2 - y1);
+            return new Rectangle(x, y, w, h);
+        }
+
+        private void panel1_MouseUp_1(object sender, MouseEventArgs e)
+        {
+            //save new drawing
+            drawings.Add(new PreviousShapes
+            {
+                type = currentType,
+                X1 = oldX,
+                Y1 = oldY,
+                X2 = e.X,
+                Y2 = e.Y,
+                color = GetColor()
+            });
+
+            //repaint panel
+            panel1.Invalidate();
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            foreach (var d in drawings)
+            {
+                using (Pen pen = new Pen(d.color))
+                {
+
+                    if (d.type == ShapeType.line)
+                    {
+                        e.Graphics.DrawLine(pen, d.X1, d.Y1, d.X2, d.Y2);
+                    }
+                    else if (d.type == ShapeType.rectangle)
+                    {
+                        e.Graphics.DrawRectangle(pen, MakeRect(d.X1, d.Y1, d.X2, d.Y2));
+                    }
+                    else
+                    {
+                        e.Graphics.DrawEllipse(pen, MakeRect(d.X1, d.Y1, d.X2, d.Y2));
+                    }
+                }
+            }
+
+        }
+    }
+}
